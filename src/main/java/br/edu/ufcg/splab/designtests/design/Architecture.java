f7ba307ffcc0c1720dccd95a19aedfe9ddffe5f8
Taciano Morais Silva <tacianosilva@gmail.com>
2015-07-06 00:08:07 -0300

    Ideia inicial para representar uma arquitetura composta de módulos.

---
+++ src/main/java/br/edu/ufcg/splab/designtests/design/Architecture.java
@@ -0,0 +1,16 @@
+package br.edu.ufcg.splab.designtests.design;
+
+public class Architecture {
+
+    private String name;
+
+    public Architecture(String name) {
+        this.name = name;
+    }
+
+    public ModuleNode getModule(String string) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
