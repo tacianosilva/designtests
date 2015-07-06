4ae1a92be163f26958884dd578d36554aff5dd6d
Taciano Morais Silva <tacianosilva@gmail.com>
2015-07-06 00:11:53 -0300

    Classe de Teste de Unidade com testes básicos.

---
+++ src/test/java/br/edu/ufcg/splab/tests/designtests/DesignWizardDecoratorTest.java
@@ -0,0 +1,56 @@
+package br.edu.ufcg.splab.tests.designtests;
+
+import static org.junit.Assert.*;
+
+import java.util.Set;
+
+import org.designwizard.design.PackageNode;
+import org.designwizard.main.DesignWizard;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import br.edu.ufcg.splab.designtests.DesignWizardDecorator;
+import br.edu.ufcg.splab.designtests.design.Architecture;
+import br.edu.ufcg.splab.designtests.design.ModuleNode;
+
+public class DesignWizardDecoratorTest {
+
+    DesignWizard dw;
+    DesignWizardDecorator dwd;
+    String projectName = "sigest";
+    String arquivoJar = "jars/sigest.jar";
+
+    @Before
+    public void setUp() throws Exception {
+        dwd = new DesignWizardDecorator(arquivoJar, projectName);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        dw = null;
+        dwd = null;
+    }
+
+    @Test
+    public final void testNewDesignWizardDecorator() {
+        dw = dwd.getDesignWizard();
+        assertNotNull("1", dw);
+
+        Set<PackageNode> packages = dw.getAllPackages();
+        assertEquals("2", 9, packages.size());
+
+        for (PackageNode packageNode : packages) {
+            System.out.println("package" + packageNode);
+        }
+
+        assertNotNull("3", dwd);
+    }
+
+    @Test
+    public final void testGetArchtecture() {
+        Architecture arch = dwd.getArchitecture();
+        ModuleNode model = arch.getModule("model");
+        assertNotNull("1", dwd);
+    }
+}
