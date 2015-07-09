7d9dac1cae15f39028713ad17fd50a1e2f2ccd2a
Taciano Morais Silva <tacianosilva@gmail.com>
2015-07-09 12:01:58 -0300

    Arquivo de main para testes utilizando anotações da versão do designwizard.

--- src/main/java/br/edu/ufcg/splab/designtests/Main.java
+++ src/main/java/br/edu/ufcg/splab/designtests/Main.java
@@ -3,11 +3,13 @@ package br.edu.ufcg.splab.designtests;
 import java.io.IOException;
 import java.util.Set;
 
+import org.designwizard.design.AnnotationNode;
 import org.designwizard.design.ClassNode;
+import org.designwizard.exception.InexistentEntityException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InexistentEntityException {
         // TODO Auto-generated method stub
 
         DesignWizardDecorator dwd;
@@ -16,11 +18,33 @@ public class Main {
 
         dwd = new DesignWizardDecorator(arquivoJar, projectName);
 
-        Set<ClassNode> classes = dwd.getClassesByEntityAnnotation();
+        Set<ClassNode> classes = dwd.getClassesByAnnotation("javax.persistence.Entity");
 
         for (ClassNode classNode : classes) {
             System.out.println(classNode.getClassName());
+            System.out.println(classNode.getAllAnnotations());
         }
+
+        Set<AnnotationNode> annotations = dwd.getAllAnnotations();
+
+        for (AnnotationNode annotationNode : annotations) {
+            System.out.println("Annotations: "+ annotationNode.getName());
+            System.out.println("Annotations: "+ annotationNode.getShortName());
+            System.out.println("Annotations: "+ annotationNode.getClassName());
+            System.out.println("Annotations: "+ annotationNode.getClassesAnnotated());
+        }
+
+        ClassNode curso = dwd.getClass("br.ufrn.cerescaico.bsi.sigest.model.Curso");
+        System.out.println("Classe >>>> ");
+        System.out.println("Name: " + curso.getName());
+        System.out.println("ClassName: " + curso.getClassName());
+        System.out.println("All Annotations: " + curso.getAllAnnotations());
+
+        AnnotationNode annotation = dwd.getAnnotation("javax.persistence.Entity");
+        System.out.println("Annotations >>>> ");
+        System.out.println("Name: " + annotation.getName());
+        System.out.println("ClassName: " + annotation.getClassName());
+        System.out.println("All Classes: " + annotation.getClassesAnnotated());
 
     }
 
