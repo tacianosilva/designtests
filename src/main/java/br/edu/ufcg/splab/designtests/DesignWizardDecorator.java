8c8589207c8132444aba3b3b7a272d4552252c40
Taciano Morais Silva <tacianosilva@gmail.com>
2015-07-09 12:00:44 -0300

    Nova versão do Decorator para DesignWizard com utilização de Anotações.

--- src/main/java/br/edu/ufcg/splab/designtests/DesignWizardDecorator.java
+++ src/main/java/br/edu/ufcg/splab/designtests/DesignWizardDecorator.java
@@ -1,17 +1,12 @@
 package br.edu.ufcg.splab.designtests;
 
 import java.io.IOException;
-import java.lang.annotation.Annotation;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
 
+import org.designwizard.design.AnnotationNode;
 import org.designwizard.design.ClassNode;
-import org.designwizard.design.Entity;
+import org.designwizard.exception.InexistentEntityException;
 import org.designwizard.main.DesignWizard;
 
 import br.edu.ufcg.splab.designtests.design.Architecture;
@@ -20,12 +15,10 @@ public class DesignWizardDecorator {
 
     private DesignWizard dw;
     private String projectName;
-    private String pathToJar;
 
     public DesignWizardDecorator(String pathToJar, String projectName) throws IOException, ClassNotFoundException {
         this.dw = new DesignWizard(pathToJar);
         this.projectName = projectName;
-        this.pathToJar = pathToJar;
     }
 
     public Architecture getArchitecture() {
@@ -37,50 +30,46 @@ public class DesignWizardDecorator {
         return dw;
     }
 
-    @SuppressWarnings("finally")
-    public Set<ClassNode> getClassesByEntityAnnotation() throws IOException {
-        JarFile jarFile = new JarFile(pathToJar);
-        Enumeration e = jarFile.entries();
+    public Set<ClassNode> getClassesByAnnotation(String annotationName) throws IOException, InexistentEntityException {
 
-        URL[] urls = { new URL("jar:file:" + pathToJar + "!/") };
-        URLClassLoader cl = URLClassLoader.newInstance(urls);
+        Set<ClassNode> allClasses = dw.getAllClasses();
+        AnnotationNode annotationNode = dw.getAnnotation(annotationName);
 
         Set<ClassNode> classes = new HashSet<ClassNode>();
 
-        while (e.hasMoreElements()) {
-            JarEntry je = (JarEntry) e.nextElement();
-            if (je.isDirectory() || !je.getName().endsWith(".class")) {
-                continue;
-            }
-            String className = je.getName().substring(0,je.getName().length()-6);
-            className = className.replace('/', '.');
-            cl = URLClassLoader.newInstance(urls);
-            Class<?> aClass;
-            try {
-                aClass = cl.loadClass(className);
-                if (hasAnnotation(aClass)) {
-                    //classes.add(aClass);
-                    classes.add(dw.getClass(aClass));
-                }
-            } catch (ClassNotFoundException ex) {
-                //ex.printStackTrace();
-            } finally {
-                cl.close();
-                cl = URLClassLoader.newInstance(urls);
-                continue;
+        for (ClassNode classNode : allClasses) {
+            if (hasAnnotation(classNode, annotationNode)) {
+                classes.add(classNode);
             }
         }
         return classes;
     }
 
-    public Boolean hasAnnotation(Class aClass) {
-        Annotation[] annotations = aClass.getAnnotations();
+    public Boolean hasAnnotation(ClassNode aClass, AnnotationNode annotation) {
+        Set<AnnotationNode> annotations = aClass.getAllAnnotations();
 
-        for (Annotation annotation : annotations) {
-            if (annotation instanceof javax.persistence.Entity) {
+        if (annotations.contains(annotation)) {
                 return true;
             }
-        }
         return false;
+    }
+
+    public Set<ClassNode> getClassesAnnotated(String annotationName) {
+        Set<ClassNode> classes = new HashSet<ClassNode>();
+        dw.getAllClasses();
+        return classes;
+    }
+
+    public Set<AnnotationNode> getAllAnnotations() {
+        Set<AnnotationNode> annotations = dw.getAllAnnotations();
+        return annotations;
+    }
+
+    public ClassNode getClass(String className) throws InexistentEntityException {
+        return dw.getClass(className);
+    }
+
+    public AnnotationNode getAnnotation(String annotationName) throws InexistentEntityException {
+        return dw.getAnnotation(annotationName);
     }
 }
