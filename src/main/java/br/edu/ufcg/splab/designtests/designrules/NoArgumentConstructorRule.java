b4e30ddad66d8d3998dedd044774558db1dcd7ea
Taciano Morais Silva <tacianosilva@gmail.com>
2015-08-13 16:16:20 -0300

    8: Implementar Regra de Design NoArgumentConstructorRule #closed #8
    Task-Url: https://gitlab.com/tacianosilva/designtests/issues/8

---
+++ src/main/java/br/edu/ufcg/splab/designtests/designrules/NoArgumentConstructorRule.java
@@ -0,0 +1,58 @@
+package br.edu.ufcg.splab.designtests.designrules;
+
+import java.util.Collection;
+import java.util.Set;
+
+import org.designwizard.design.ClassNode;
+import org.designwizard.design.MethodNode;
+import org.designwizard.designrules.Rule;
+
+import br.edu.ufcg.splab.designtests.DesignWizardDecorator;
+
+/**
+ *
+ * Rule: A default constructor must be implemented in classes of the Model Package.
+ * All persistent classes must have a default constructor (which can be non-public)
+ * so that Hibernate can instantiate them using java.lang.reflect.Constructor.newInstance().
+ *
+ * @author Taciano
+ */
+public class NoArgumentConstructorRule extends AbstractDesignRule implements Rule {
+
+    public NoArgumentConstructorRule(DesignWizardDecorator dwd) {
+        super(dwd);
+    }
+
+    @Override
+    public boolean checkRule() {
+        Collection<ClassNode> classes = getClassNodes();
+
+        for (ClassNode entityNode : classes) {
+
+            Set<MethodNode> constructors = entityNode.getConstructors();
+            boolean passed = false;
+
+            for (MethodNode methodNode : constructors) {
+                //TODO Será necessário verificar os métodos herdados?
+                Set<ClassNode> parameters = methodNode.getParameters();
+                if (parameters.isEmpty()) {
+                    passed = true;
+                    break;
+                }
+            }
+
+            if (!passed) {
+                this.report += "The class <" + entityNode.getName() + "> doesn't contain a default constructor.\n";
+                addResultFalse(entityNode);
+            } else {
+                addResultTrue(entityNode);
+            }
+        }
+        return this.report.equals("") ? true : false;
+    }
+
+    @Override
+    public String getReport() {
+        return this.report;
+    }
+}
