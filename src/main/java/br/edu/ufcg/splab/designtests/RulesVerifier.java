219c777b34d34a5015507e8e391d6c2e38dc913b
Taciano Morais Silva <tacianosilva@gmail.com>
2015-08-27 11:27:33 -0300

    Geração simplificada de resultados e adição de novas regras.

--- src/main/java/br/edu/ufcg/splab/designtests/RulesVerifier.java
+++ src/main/java/br/edu/ufcg/splab/designtests/RulesVerifier.java
@@ -5,12 +5,19 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashSet;
 import java.util.Set;
 
 import org.designwizard.design.ClassNode;
 import org.designwizard.exception.InexistentEntityException;
 
+import br.edu.ufcg.splab.designtests.designrules.AbstractDesignRule;
+import br.edu.ufcg.splab.designtests.designrules.HashCodeAndEqualsRule;
 import br.edu.ufcg.splab.designtests.designrules.ImplementsSerializableRule;
+import br.edu.ufcg.splab.designtests.designrules.NoArgumentConstructorRule;
+import br.edu.ufcg.splab.designtests.designrules.NoFinalClassRule;
+import br.edu.ufcg.splab.designtests.designrules.ProvideIdentifierPropertyRule;
+import br.edu.ufcg.splab.designtests.designrules.UseSetCollectionRule;
 
 public class RulesVerifier {
 
@@ -18,7 +25,7 @@ public class RulesVerifier {
         System.out.printf("\nConteúdo do arquivo projectsThatUseHibernate.txt\n\n");
 
         String fileName = "scripts/projectsThatUseHibernate_2015-08-10.txt";
-        String fileResults = "scripts/results_serializable_2015-08-12.txt";
+        String fileResults = "scripts/results_2015-08-20.txt";
         processarArquivo(fileName, fileResults);
 
     }
@@ -30,7 +37,7 @@ public class RulesVerifier {
 
             FileWriter fw = criarArquivo(fileResults);
             PrintWriter gravarArq = new PrintWriter(fw);
-            gravarArq.printf("%s, %s, %s, %s, %s, %s, %s, %s\n", "gitUser", "projectName", "numEntidades", "rulecheck", "totalPassou", "totalFalhou", "ocorreuErro", "msgErro");
+            gravarArq.printf("%s,%s,%s,%s\n", "project", "class", "rule", "result");
             String linha = lerArq.readLine(); // lê a primeira linha
             // a variável "linha" recebe o valor "null" quando o processo
             // de repetição atingir o final do arquivo texto
@@ -51,60 +58,72 @@ public class RulesVerifier {
 
     public static void processarProjeto(String projeto, PrintWriter gravar) {
         String[] split = projeto.split("/");
-        String gitUser = split[0];
+        //String gitUser = split[0];
         String projectName = split[1];
         String reposDir = "scripts/repos/";
         String classDir = "/target/classes";
 
         String projectDir = reposDir + projeto + classDir;
-        boolean rulecheck = false;
-        boolean ocorreuErro = false;
-        String msgErro = "Não houve erro!";
-        int numEntidades = 0;
-        int totalPassou = 0;
-        int totalFalhou = 0;
 
         try {
             System.out.println("Diretório do Projeto: " + projectDir);
             DesignWizardDecorator dwd = new DesignWizardDecorator(projectDir, projectName);
 
             Set<ClassNode> classes = dwd.getClassesByAnnotation("javax.persistence.Entity");
-            numEntidades = classes.size();
 
-            ImplementsSerializableRule rule = new ImplementsSerializableRule(dwd);
-            rule.setClassNodes(classes);
-            if (!rule.checkRule()) {
-                System.out.println("Report Rule");
-                System.out.println(rule.getReport());
-            } else {
-                rulecheck = true;
+            Set<AbstractDesignRule> regras = getRegras(dwd);
+
+            for (ClassNode classNode : classes) {
+                for (AbstractDesignRule rule : regras) {
+                    rule.setClassNode(classNode);
+                    String ruleName = rule.getName();
+                    boolean checkResult = rule.checkRule();
+
+                    System.out.println("Report Rule" + ruleName);
+                    System.out.println(rule.getReport());
+
+                    System.out.println(">>>>" + projeto + ", " + classNode.getClassName() + ", " + ruleName + ", " + checkResult);
+
+                    gravarLinha(gravar, projeto, classNode.getClassName(), ruleName, checkResult);
+                }
             }
-            totalPassou = rule.getResultsTrue().size();
-            totalFalhou = rule.getResultsFalse().size();
-
         } catch (IOException ioe) {
-            // TODO Auto-generated catch block
-            ocorreuErro = true;
-            msgErro = ioe.getMessage();
             ioe.printStackTrace();
         } catch (ClassNotFoundException ce) {
-            // TODO Auto-generated catch block
-            ocorreuErro = true;
-            msgErro = ce.getMessage();
             ce.printStackTrace();
         } catch (InexistentEntityException e) {
-            // TODO Auto-generated catch block
-            ocorreuErro = true;
-            msgErro = e.getMessage();
             e.printStackTrace();
         } catch (ArrayIndexOutOfBoundsException e) {
-            ocorreuErro = true;
-            msgErro = e.getMessage();
             e.printStackTrace();
         }
-        if (!ocorreuErro) {
-            gravarLinha(gravar, gitUser, projectName, numEntidades, rulecheck, totalPassou, totalFalhou, ocorreuErro, msgErro);
-        }
+    }
+
+    private static void gravarLinha(PrintWriter gravar, String projeto, String className, String ruleName,
+            boolean checkResult) {
+        gravar.printf("%s,%s,%s,%s\n", projeto, className, ruleName, checkResult);
+    }
+
+    private static Set<AbstractDesignRule> getRegras(DesignWizardDecorator dwd) {
+        Set<AbstractDesignRule> regras = new HashSet<AbstractDesignRule>();
+
+        //Regras extraídas do manual
+        AbstractDesignRule rule1 = new HashCodeAndEqualsRule(dwd);
+        regras.add(rule1);
+        AbstractDesignRule rule2 = new NoArgumentConstructorRule(dwd);
+        regras.add(rule2);
+        AbstractDesignRule rule3 = new ProvideIdentifierPropertyRule(dwd);
+        regras.add(rule3);
+        AbstractDesignRule rule4 = new NoFinalClassRule(dwd);
+        regras.add(rule4);
+        //AbstractDesignRule rule5 = new ProvideGetsSetsFieldsRule(dwd);
+        //regras.add(rule5);
+
+        AbstractDesignRule rule6 = new UseSetCollectionRule(dwd);
+        regras.add(rule6);
+        AbstractDesignRule rule7 = new ImplementsSerializableRule(dwd);
+        regras.add(rule7);
+
+        return regras;
     }
 
     public static void gravarLinha(PrintWriter gravar, String gitUser, String projectName, int numEntidades,
