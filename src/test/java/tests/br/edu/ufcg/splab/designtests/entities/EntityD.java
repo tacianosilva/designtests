ffb7f5bcf59c75cb984cacf9c541eae899ef51d1
Taciano Morais Silva <tacianosilva@gmail.com>
2015-10-02 11:50:12 -0300

    add new entity for tests.

---
+++ src/test/java/tests/br/edu/ufcg/splab/designtests/entities/EntityD.java
@@ -0,0 +1,29 @@
+package tests.br.edu.ufcg.splab.designtests.entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class EntityD implements Serializable {
+
+    @Column
+    private Integer id;
+
+    @Column
+    private String name;
+
+    @Column
+    private List<String> collectionList;
+
+    @Column
+    private ArrayList<String> collectionArrayList;
+
+    @Column
+    private LinkedList<String> collectionLinkedList;
+}
