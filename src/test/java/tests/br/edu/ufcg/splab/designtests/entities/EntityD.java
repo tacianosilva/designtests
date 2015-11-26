ed1bdea76821f209a625e8b9d3b9bece06ef1808
Taciano Morais Silva <tacianosilva@gmail.com>
2015-11-26 20:44:56 -0300

    update!

--- src/test/java/tests/br/edu/ufcg/splab/designtests/entities/EntityD.java
+++ src/test/java/tests/br/edu/ufcg/splab/designtests/entities/EntityD.java
@@ -57,12 +57,4 @@ public class EntityD implements Serializable {
     public void setName(String name) {
         this.name = name;
     }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
-    }
 }
