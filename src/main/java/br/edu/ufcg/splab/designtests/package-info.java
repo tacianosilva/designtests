ed4dd88791c168736c37ab59bbe0817955939f4d
Taciano Morais Silva <tacianosilva@gmail.com>
2016-01-27 09:09:14 -0300

    Deleted tests files that they were moved.

--- src/main/java/br/edu/ufcg/splab/designtests/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Package with a main class for execution of desing rules for a software's design.
- * @author Taciano Morais Silva - tacianosilva@gmail.com
- */
-package br.edu.ufcg.splab.designtests;
