4b9c36bf44c23d9ce715466e1fc5fbdb49dfaecc
Taciano Morais Silva <tacianosilva@gmail.com>
2016-03-21 14:01:52 -0300

    Removed not used imports.

--- src/test/java/tests/br/edu/ufcg/splab/designtests/designrules/ProvideIdentifierPropertyRuleTest.java
+++ src/test/java/tests/br/edu/ufcg/splab/designtests/designrules/ProvideIdentifierPropertyRuleTest.java
@@ -1,8 +1,6 @@
 package tests.br.edu.ufcg.splab.designtests.designrules;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Set;
